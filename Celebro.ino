3ede53264d06c59adccc85acd4b7784be692aac7
flixgamerd <flixgamerd@gmail.com>
2026-03-08 22:34:58 +0100

    feat: implementacao de sensor de pH com trava de seguranca e dual-pin

--- Celebro.ino
+++ Celebro.ino
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 //---Variaveis---pra definir os pinos 
-const int pinoSensor = 34;
+const int pinoSalinidade = 34;
+const int pinoPH = 35;//novo pino adicionado
 //funcoes de calculo
 
   float converterLeitura(int leitura)
@@ -13,30 +14,79 @@ const int pinoSensor = 34;
     return ((float)(leitura - rawMin) / (rawMax - rawMin)) * (fisMax - fisMin) + fisMin;
   }
 
-//funcao principal
-void processarSalinizacao()
-{
-  int leitura = analogRead(pinoSensor);
+//----calculo de PH da agua----
+//definicoes para o sensor
+const float PH_Neutro = 2.5;//voltagem para quando o ph for 7.0
+const float PH_Passo = 0.18;//Sensibilidade do sensor por unidade de PH
 
-float valorFinal = converterLeitura(leitura);
+float CalcularPH(int leituraBruta)
+{
+  //Converter a leitura do ADC para Voltagem
+  float voltagem = (leituraBruta * 3.3) / 4095.0;
+
+  //Calcula o valor de PH 
+  float valorPH = 7.0 + ((PH_Neutro - voltagem) / PH_Passo);
+
+  return valorPH;
+}
+
+//funcao principal
+void processarMonitoramento()
+{
+  int leituraSal = analogRead(pinoSalinidade);
+  int leituraPH = analogRead(pinoPH); 
+
+float valorSal = converterLeitura(leituraSal);
+float valorPH = CalcularPH(leituraPH);
+//trava de seguraca de PH para evitar valores loucos * novo
+if (valorPH > 14.0) valorPH = 14.0;
+if (valorPH < 0.0) valorPH = 0.0;
+
 
 Serial.print("Valor Final: ");
-    Serial.println(valorFinal);
+    Serial.println(valorSal);
     Serial.println(" mg/L");
-
+    Serial.println ("Nivel de PH");
+    Serial.println (valorPH);
 }
 //Funcao de conversa entre o codigo e o Arduino
 
 void setup()
 {
     Serial.begin(115200);//inicia a conversa
-    pinMode(pinoSensor, INPUT);
+    pinMode(pinoSalinidade, INPUT);
+    pinMode(pinoPH, INPUT);
 }
 
 void loop()
 {
   //executa um loop infinito
-  processarSalinizacao();
+  processarMonitoramento();
   delay(1000);//espera 1 segundo entre as leituras
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//criado por: Flix codes.
